ebc88d6f3db443925f21a015e5275277d8b4047a
ohsangsoo <tkdtn3264@gmail.com>
2023-04-22 21:44:19 +0900

    //서블릿이 호출되면 서비스 메서드 가 호출된다.

--- src/main/java/oss/servlet/basic/HelloServlet.java
+++ src/main/java/oss/servlet/basic/HelloServlet.java
@@ -1,7 +1,17 @@
 package oss.servlet.basic;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 @WebServlet (name = "helloServlet",urlPatterns = "/hello")
 public class HelloServlet extends HttpServlet { // HttpServlet를 상속받는다.
+
+    @Override //서블릿이 호출되면 서비스 메서드 가 호출된다.
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.service(req, resp);
+    }
 }
