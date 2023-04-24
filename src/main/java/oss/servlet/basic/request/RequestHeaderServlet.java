f79c610c074a6ff88626f0b6dd4b56730a9cf961
ohsangsoo <tkdtn3264@gmail.com>
2023-04-24 16:24:10 +0900

    //Header  모든 정보를 다른 방법으로 출하기

--- src/main/java/oss/servlet/basic/request/RequestHeaderServlet.java
+++ src/main/java/oss/servlet/basic/request/RequestHeaderServlet.java
@@ -39,12 +39,15 @@ public class RequestHeaderServlet  extends HttpServlet {
     private void printHeaders(HttpServletRequest request) {
         System.out.println("--- Headers - start ---");
         // 헤더를 가져오는 방법이 두가지 있는데
-        Enumeration<String> headerNames = request.getHeaderNames(); //request.getHeaderNames http 헤더 메시지에 있는 모든 정보를 다꺼내서 다출력할수있다.
-        while(headerNames.hasMoreElements()){ // headerNames 동작했을때 다음요소 있는가?
-            String headerName = headerNames.nextElement();// 다음 요소가 있다면 값을 꺼내라
-            System.out.println(headerName + " : " + headerName);//  값을 출력해라.
-
-        }
+        // 1.헤더 가져오기
+//        Enumeration<String> headerNames = request.getHeaderNames(); //request.getHeaderNames http 헤더 메시지에 있는 모든 정보를 다꺼내서 다출력할수있다.
+//        while(headerNames.hasMoreElements()){ // headerNames 동작했을때 다음요소 있는가?
+//            String headerName = headerNames.nextElement();// 다음 요소가 있다면 값을 꺼내라
+//            System.out.println(headerName + " : " + headerName);//  값을 출력해라.
+//        }
+        // 2. 헤더가져오기 간결하게 쓸수있다.
+            request.getHeaderNames().asIterator() // asIterator로 바꿀수있다.
+                    .forEachRemaining(headerName -> System.out.println(headerName + " : " + headerName));
         System.out.println("--- Headers - end ---");
         System.out.println();
     }
