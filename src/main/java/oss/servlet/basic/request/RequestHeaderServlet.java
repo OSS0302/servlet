87ee441ce276bba2bc8245d9109f7a0f33fcedc8
ohsangsoo <tkdtn3264@gmail.com>
2023-04-24 15:48:26 +0900

    스타트 라인정보 http 메서드 http 요청 메시지에 가장  첫라인 정보를 불러오는 걸 나오는지 테스트 하기

---
+++ src/main/java/oss/servlet/basic/request/RequestHeaderServlet.java
@@ -0,0 +1,35 @@
+package oss.servlet.basic.request;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "requestHeaderServlet",urlPatterns = "/request-header")
+public class RequestHeaderServlet  extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 스타트 라인정보 http 메서드 http 요청 메시지에 가장  첫라인 정보를 불러오는 걸 만들겠다.
+        printStartLine(request);
+    }
+
+    private static void printStartLine(HttpServletRequest request) {
+        System.out.println("--- REQUEST-LINE - start ---");
+        System.out.println("request.getMethod() = " + request.getMethod()); //GET
+        System.out.println("request.getProtocol() = " + request.getProtocol()); //HTTP/1.1
+        System.out.println("request.getScheme() = " + request.getScheme()); //http
+        // http://localhost:8080/request-header
+        System.out.println("request.getRequestURL() = " + request.getRequestURL());
+        // /request-header
+        System.out.println("request.getRequestURI() = " + request.getRequestURI());
+        //username=hi
+        System.out.println("request.getQueryString() = " +
+                request.getQueryString());
+        System.out.println("request.isSecure() = " + request.isSecure()); //https 사용 유무
+        System.out.println("--- REQUEST-LINE - end ---");
+        System.out.println();
+    }
+}
