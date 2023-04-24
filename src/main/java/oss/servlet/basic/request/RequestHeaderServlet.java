6ceb486d7f16589d8bf41f6c43a231ca3919f019
ohsangsoo <tkdtn3264@gmail.com>
2023-04-24 16:05:33 +0900

    //Header 모든 정보 출력하기

--- src/main/java/oss/servlet/basic/request/RequestHeaderServlet.java
+++ src/main/java/oss/servlet/basic/request/RequestHeaderServlet.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Enumeration;
 
 @WebServlet(name = "requestHeaderServlet",urlPatterns = "/request-header")
 public class RequestHeaderServlet  extends HttpServlet {
@@ -14,6 +15,8 @@ public class RequestHeaderServlet  extends HttpServlet {
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // 스타트 라인정보 http 메서드 http 요청 메시지에 가장  첫라인 정보를 불러오는 걸 만들겠다.
         printStartLine(request);
+        printHeaders(request);
+
     }
 
     private static void printStartLine(HttpServletRequest request) {
@@ -32,4 +35,18 @@ public class RequestHeaderServlet  extends HttpServlet {
         System.out.println("--- REQUEST-LINE - end ---");
         System.out.println();
     }
+    //Header 모든 정보
+    private void printHeaders(HttpServletRequest request) {
+        System.out.println("--- Headers - start ---");
+        // 헤더를 가져오는 방법이 두가지 있는데
+        Enumeration<String> headerNames = request.getHeaderNames(); //request.getHeaderNames http 헤더 메시지에 있는 모든 정보를 다꺼내서 다출력할수있다.
+        while(headerNames.hasMoreElements()){ // headerNames 동작했을때 다음요소 있는가?
+            String headerName = headerNames.nextElement();// 다음 요소가 있다면 값을 꺼내라
+            System.out.println(headerName + " : " + headerName);//  값을 출력해라.
+
+        }
+        System.out.println("--- Headers - end ---");
+        System.out.println();
+    }
+
 }
