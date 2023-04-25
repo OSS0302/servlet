14a0e830355a9f07c59636c925e75624214ec35a
ohsangsoo <tkdtn3264@gmail.com>
2023-04-25 15:17:49 +0900

    JSON 결과를 파싱해서 사용할 수 있는 자바 객체로 변환하려면 Jackson, Gson 같은 JSON 변환

--- src/main/java/oss/servlet/basic/request/RequestBodyJsonServlet.java
+++ src/main/java/oss/servlet/basic/request/RequestBodyJsonServlet.java
@@ -1,5 +1,6 @@
 package oss.servlet.basic.request;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.util.StreamUtils;
 import org.springframework.util.StringUtils;
 
@@ -15,6 +16,9 @@ import java.nio.charset.StandardCharsets;
 @WebServlet(name = "requestBodyJsonServlet", urlPatterns = "/request-body-json")
 public class RequestBodyJsonServlet  extends HttpServlet {
 
+    // HelloData 클래스안에 username 과 age을 사용하려면 잭슨 을 이용하면 됩니다.
+    private ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         ServletInputStream inputStream = request.getInputStream();
@@ -23,8 +27,14 @@ public class RequestBodyJsonServlet  extends HttpServlet {
         // 출력하기
         System.out.println("messageBody = " + messageBody);
 
+
+
+        HelloData helloData = objectMapper.readValue(messageBody, HelloData.class);
+
+        System.out.println("helloData.getUsername() = " + helloData.getUsername());
+        System.out.println("helloData.getAge() = " + helloData.getAge());
+
         response.getWriter().write("ok");
-        //포스트맨 으로 확인하기
 
     }
 }
