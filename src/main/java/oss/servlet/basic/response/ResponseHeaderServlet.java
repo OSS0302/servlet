ea32c166e0002a9f69481105df2f1507ed0b1b69
ohsangsoo <tkdtn3264@gmail.com>
2023-04-25 16:26:50 +0900

    Content 편의 메서드 추가하기

--- src/main/java/oss/servlet/basic/response/ResponseHeaderServlet.java
+++ src/main/java/oss/servlet/basic/response/ResponseHeaderServlet.java
@@ -14,15 +14,25 @@ public class ResponseHeaderServlet extends HttpServlet {
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     // [state-line]
     response.setStatus(HttpServletResponse.SC_OK);
+
     //[state-headers]
     response.setHeader("content-Type", "text/plain;charset = utf-8");
     response.setHeader("Cache-Control","no-cache, no-store, must-revalidate" );// 캐쉬를 무효화 하겠다 .
     response.setHeader("Pragma","no-cache");// 과거버전까지 캐쉬를 없애겠다.
     response.setHeader("my-header","hello");// 임의의 헤더 을  넣겠다.
 
+        //[header 편의 메서드 ]
+       content(response);
     // 테스트
         PrintWriter writer = response.getWriter();
         writer.println("ok");
 
+    }
+    private void content(HttpServletResponse response) {
+        //Content-Type: text/plain;charset=utf-8
+        //Content-Length: 2
+        response.setHeader("Content-Type", "text/plain;charset=utf-8"); response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        // response.setContentLength(2); //(생략시 자동 생성)
     }
 }
