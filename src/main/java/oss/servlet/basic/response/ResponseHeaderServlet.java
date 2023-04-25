5edac68405be1f8e9cf676f3016f48c0ad68c24f
ohsangsoo <tkdtn3264@gmail.com>
2023-04-25 16:21:12 +0900

    HttpServletResponse - 기본 사용하는 방법

---
+++ src/main/java/oss/servlet/basic/response/ResponseHeaderServlet.java
@@ -0,0 +1,28 @@
+package oss.servlet.basic.response;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name ="responseHeaderServlet",urlPatterns = "/response-header")
+public class ResponseHeaderServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    // [state-line]
+    response.setStatus(HttpServletResponse.SC_OK);
+    //[state-headers]
+    response.setHeader("content-Type", "text/plain;charset = utf-8");
+    response.setHeader("Cache-Control","no-cache, no-store, must-revalidate" );// 캐쉬를 무효화 하겠다 .
+    response.setHeader("Pragma","no-cache");// 과거버전까지 캐쉬를 없애겠다.
+    response.setHeader("my-header","hello");// 임의의 헤더 을  넣겠다.
+
+    // 테스트
+        PrintWriter writer = response.getWriter();
+        writer.println("ok");
+
+    }
+}
