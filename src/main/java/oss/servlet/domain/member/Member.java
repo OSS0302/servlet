abdfbbc6102d158758c58e969a848fc57deb53fe
ohsangsoo <tkdtn3264@gmail.com>
2023-04-26 19:04:26 +0900

    멤버 클래스 생성하기

---
+++ src/main/java/oss/servlet/domain/member/Member.java
@@ -0,0 +1,20 @@
+package oss.servlet.domain.member;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Member {
+
+    private Long id ; // 회원아이디
+    private String username; //회원 이름
+    private Long age ; // 나이
+
+    public Member() { // 기본생성자
+    }
+
+    public Member(String username, Long age) {
+        this.username = username;
+        this.age = age;
+    }
+}
