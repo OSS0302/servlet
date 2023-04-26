836955d34424e30e978b0c25df43a5d8afc47839
ohsangsoo <tkdtn3264@gmail.com>
2023-04-26 19:59:52 +0900

    회원 이름과 나이 전송하는 html 만들었다.

---
+++ src/main/java/oss/servlet/web/servlet/MemberFormServlet.java
@@ -0,0 +1,37 @@
+package oss.servlet.web.servlet;
+
+import oss.servlet.domain.member.MemberRepository;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "memberFormServlet", urlPatterns = "/servlet/members/new-form")
+public class MemberFormServlet extends HttpServlet {
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+    //private MemberRepository memberRepository = new MemberRepository(); 할 수없다 기본 생성자로 막았다.
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html"); //  html 형식을 쓸거다.
+        response.setCharacterEncoding("utf-8"); //utf-8
+
+        PrintWriter w = response.getWriter();
+        w.write("<!DOCTYPE html>\n" +
+                "<html>\n" +
+                "<head>\n" +
+                "    <meta charset=\"UTF-8\">\n" +
+                "    <title>Title</title>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "<form action=\"/servlet/members/save\" method=\"post\">\n" +
+                "    username: <input type=\"text\" name=\"username\" />\n" +
+                "    age:      <input type=\"text\" name=\"age\" />\n" +
+                " <button type=\"submit\">전송</button>\n" + "</form>\n" +
+                "</body>\n" +
+                "</html>\n");
+    }
+}
