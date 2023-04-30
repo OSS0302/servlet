2d67071301738e887cede161525a9bef2497c5b7
ohsangsoo <tkdtn3264@gmail.com>
2023-04-30 19:34:45 +0900

    ControllerV1 인터페이스 컨트롤러

---
+++ src/main/java/oss/servlet/web/frontcontroller/v1/controllerV1.java
@@ -0,0 +1,12 @@
+package oss.servlet.web.frontcontroller.v1;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public interface controllerV1 {
+
+    void process(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException;
+}
