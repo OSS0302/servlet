b4e69656909ce9c86e371778e77d4430888a5f1e
ohsangsoo <tkdtn3264@gmail.com>
2023-04-30 19:36:48 +0900

    ControllerV1 인터페이스 컨트롤러 다형성을 이용해서 편하게 쓰려고 인터페이스 만들었다.

--- src/main/java/oss/servlet/web/frontcontroller/v1/ControllerV1.java
+++ src/main/java/oss/servlet/web/frontcontroller/v1/ControllerV1.java
@@ -5,7 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public interface controllerV1 {
+public interface ControllerV1 {
 
     void process(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException;
