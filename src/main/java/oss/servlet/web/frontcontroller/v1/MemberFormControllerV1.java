b06d9db335d6a94894b50f7637931c3c16f0fcd5
ohsangsoo <tkdtn3264@gmail.com>
2023-04-30 19:46:49 +0900

    MemberFormControllerV1 - 회원 등록 컨트롤러 생성하기

--- src/main/java/oss/servlet/web/frontcontroller/v1/MemberFormControllerV1.java
+++ src/main/java/oss/servlet/web/frontcontroller/v1/MemberFormControllerV1.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class MemberControllerV1 implements ControllerV1 {
+public class MemberFormControllerV1 implements ControllerV1 {
 
     @Override
     public void process(HttpServletRequest request, HttpServletResponse
