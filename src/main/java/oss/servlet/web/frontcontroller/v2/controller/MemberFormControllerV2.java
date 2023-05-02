6fced9511cc96c335ca78282107e6d3db9ae785d
ohsangsoo <tkdtn3264@gmail.com>
2023-05-02 19:03:10 +0900

    MemberFormController V2 - 회원 등록 폼 컨트롤러 생성하기

--- src/main/java/oss/servlet/web/frontcontroller/v2/controller/MemberFormControllerV2.java
+++ src/main/java/oss/servlet/web/frontcontroller/v2/controller/MemberFormControllerV2.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class MemberFormControllerV2 implements ControllerV2 {
-    // 중복이 제거 코드가 간결해진다.
+    // 중복이 제거 코드가 간결해진다.MyView 객체를 생성하고 거기에 뷰 이름만 넣고 반환하면 된다.
     @Override
     public MyView process(HttpServletRequest request, HttpServletResponse
             response) throws ServletException, IOException {
