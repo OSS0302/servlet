13b2f48ef7bed819c4de185672bbfa4585d4ebbd
ohsangsoo <tkdtn3264@gmail.com>
2023-05-03 16:04:01 +0900

    MemberSaveControllerV3 - 회원 저장

---
+++ src/main/java/oss/servlet/web/frontcontroller/v3/controller/MemberSaveControllerV3.java
@@ -0,0 +1,28 @@
+package oss.servlet.web.frontcontroller.v3.controller;
+
+import oss.servlet.domain.member.Member;
+import oss.servlet.domain.member.MemberRepository;
+import oss.servlet.web.frontcontroller.ModelVeiw;
+import oss.servlet.web.frontcontroller.v3.ControllerV3;
+
+import java.util.Map;
+
+public class MemberSaveControllerV3 implements ControllerV3 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+    @Override
+    public ModelVeiw process(Map<String, String> paramMap) {
+        String username = paramMap.get("username"); //데이터를 꺼내서 사용하면된다.
+        int age = Integer.parseInt(paramMap.get("age"));
+
+        Member member = new Member(username,age);
+        memberRepository.save(member); // 이름과 나이를 저장한다.
+
+        //모델 인스턴스 를 만들어서 멤버의 데이터를  modelView 를 넣는다.
+        ModelVeiw mv =new ModelVeiw("save-result");
+        mv.getModel().put("member",member);
+        return mv; // modelVeiw로 반환하다.
+
+
+    }
+}
