b4a0db80c001b85e52a8fb49ab8484f648c5e7d9
ohsangsoo <tkdtn3264@gmail.com>
2023-05-04 19:35:50 +0900

    어댑터가 해당 컨트롤러를 처리할 수 있는지 판단하는 메서드다

---
+++ src/main/java/oss/servlet/web/servlet/MyHandlerAdapter.java
@@ -0,0 +1,14 @@
+package oss.servlet.web.servlet;
+
+import oss.servlet.web.frontcontroller.ModelVeiw;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public interface MyHandlerAdapter  { //handler는 컨트롤러를 말한다. 어댑터가 해당 컨트롤러를 처리할 수 있는지 판단하는 메서드다.
+    boolean supports(Object handler);
+    ModelVeiw handle(HttpServletRequest request, HttpServletResponse response,
+                     Object handler) throws ServletException, IOException;
+}
