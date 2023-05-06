60d1dea1bde25d7b08bdec7069490b667c537a27
ohsangsoo <tkdtn3264@gmail.com>
2023-05-06 17:58:52 +0900

    @Controller 어노테이션을 사용하는것이 제일 깔끔하다.

--- src/main/java/oss/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
+++ src/main/java/oss/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 
-//@Controller
+@Controller
 //@Component //컴포넌트 스캔을 통해 스프링 빈으로 등록
-@RequestMapping
+//@RequestMapping
 public class SpringMemberFormControllerV1 {
     @RequestMapping("/springmvc/v1/members/new-form")
     public ModelAndView process(){
