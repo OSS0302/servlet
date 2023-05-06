6d89fcda4e2ce2962580dba8a9b8db32dbacac20
ohsangsoo <tkdtn3264@gmail.com>
2023-05-06 17:54:24 +0900

    @Component어노테이션을 사용해도 된다.

--- src/main/java/oss/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
+++ src/main/java/oss/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
@@ -1,11 +1,14 @@
 package oss.servlet.web.springmvc.v1;
 
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 
-@Controller
+//@Controller
+@Component //컴포넌트 스캔을 통해 스프링 빈으로 등록
+@RequestMapping
 public class SpringMemberFormControllerV1 {
     @RequestMapping("/springmvc/v1/members/new-form")
     public ModelAndView process(){
