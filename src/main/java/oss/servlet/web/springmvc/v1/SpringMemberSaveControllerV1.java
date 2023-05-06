44aeb7f9ef97ee52c1168afdb9aa5ad3eeb94c53
ohsangsoo <tkdtn3264@gmail.com>
2023-05-06 18:08:37 +0900

    회원 저장 하는 컨트롤러 생성

---
+++ src/main/java/oss/servlet/web/springmvc/v1/SpringMemberSaveControllerV1.java
@@ -0,0 +1,31 @@
+package oss.servlet.web.springmvc.v1;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import oss.servlet.domain.member.Member;
+import oss.servlet.domain.member.MemberRepository;
+import oss.servlet.web.frontcontroller.ModelVeiw;
+
+import java.util.Map;
+
+public class SpringMemberSaveControllerV1  {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/springmvc/v1/members/save")
+    public ModelAndView process(Map<String, String> paramMap) {
+        String username = paramMap.get("username"); //데이터를 꺼내서 사용하면된다.
+        int age = Integer.parseInt(paramMap.get("age"));
+
+        Member member = new Member(username,age);
+        memberRepository.save(member); // 이름과 나이를 저장한다.
+
+        //모델 인스턴스 를 만들어서 멤버의 데이터를  modelView 를 넣는다.
+        ModelAndView mv =new ModelAndView("save-result");
+        //mv.getModel().put("member",member); //이거 사용해도 되지만
+        mv.addObject("member",member);
+        return mv; // modelVeiw로 반환하다.
+
+
+    }
+}
