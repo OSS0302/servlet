4723abe57f56de440c0b31caeccec5f441a157f0
ohsangsoo <tkdtn3264@gmail.com>
2023-05-06 17:43:38 +0900

    뷰리졸버 가 논리적인 뷰이름을 넘긴다.

---
+++ src/main/java/oss/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
@@ -0,0 +1,15 @@
+package oss.servlet.web.springmvc.v1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class SpringMemberFormControllerV1 {
+    @RequestMapping("/springmvc/v1/members/new-form")
+    public ModelAndView process(){
+        return new ModelAndView("new-form"); // 뷰리졸버 가 논리적인 뷰이름을 넘긴다.
+    }
+
+}
