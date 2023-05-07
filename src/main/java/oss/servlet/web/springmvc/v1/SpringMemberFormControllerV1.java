d67a9786e61485f72391ff3ad4cd39d16944db78
ohsangsoo <tkdtn3264@gmail.com>
2023-05-07 18:19:30 +0900

    SpringMemberFormControllerV1 - 회원 등록 폼 원복 @Controller anntation을 사용한다.

--- src/main/java/oss/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
+++ src/main/java/oss/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
@@ -5,10 +5,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-//@Controller
+@Controller
 
-//@Component
-@RequestMapping//컴포넌트 스캔을 통해 스프링 빈으로 등록
+//@Component//컴포넌트 스캔을 통해 스프링 빈으로 등록
+//@RequestMapping
 public class SpringMemberFormControllerV1 {
     @RequestMapping("/springmvc/v1/members/new-form")
     public ModelAndView process(){
