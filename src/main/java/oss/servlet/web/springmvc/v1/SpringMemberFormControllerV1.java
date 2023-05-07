3572165506704c383f6d82eaff3bfbb8d9b529f7
ohsangsoo <tkdtn3264@gmail.com>
2023-05-07 18:10:56 +0900

    SpringMemberFormControllerV1 - 회원 등록 폼

--- src/main/java/oss/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
+++ src/main/java/oss/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
@@ -5,15 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-
-//@Component //컴포넌트 스캔을 통해 스프링 빈으로 등록
-//@RequestMapping
-
 public class SpringMemberFormControllerV1 {
     @RequestMapping("/springmvc/v1/members/new-form")
-    public ModelAndView process() {
-        return new ModelAndView("new-form");// 뷰리졸버 가 논리적인 뷰이름을 넘긴다.
+    public ModelAndView process(){
+        return new ModelAndView("new-form");
+
     }
-
-
 }
