0de2c605e20b5cc184cd1f329d66c866240aa50e
ohsangsoo <tkdtn3264@gmail.com>
2023-05-07 18:16:08 +0900

    SpringMemberFormControllerV1 - 회원 등록 폼 @Component를 넣고 @RequestMapping을 해도 된다.

--- src/main/java/oss/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
+++ src/main/java/oss/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
@@ -1,10 +1,14 @@
 package oss.servlet.web.springmvc.v1;
 
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+//@Controller
+
+@Component
+@RequestMapping//컴포넌트 스캔을 통해 스프링 빈으로 등록
 public class SpringMemberFormControllerV1 {
     @RequestMapping("/springmvc/v1/members/new-form")
     public ModelAndView process(){
