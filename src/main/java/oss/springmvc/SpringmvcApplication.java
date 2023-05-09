c7fcdb387e620aa8d56eaaa05c3bd4ad8ca5d47a
ohsangsoo <tkdtn3264@gmail.com>
2023-05-09 18:01:53 +0900

    SpringmvcApplication 생성

---
+++ src/main/java/oss/springmvc/SpringmvcApplication.java
@@ -0,0 +1,15 @@
+package oss.springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@ServletComponentScan // 서블릿 자동 등록
+@SpringBootApplication
+public class SpringmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmvcApplication.class, args);
+	}
+}
+
