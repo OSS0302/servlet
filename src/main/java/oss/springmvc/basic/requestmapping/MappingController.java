246af34433b4b630e2e9af0da626ffa4bc4fdb3a
ohsangsoo <tkdtn3264@gmail.com>
2023-05-10 21:08:17 +0900

    // @RequestMapping 은 URL 경로를 템플릿화 할 수 있는데, @PathVariable 을 사용하면 매칭 되는 부분을 편리하게 조회할 수 있다.@PathVariable 의 이름과 파라미터 이름이 같으면 생략할 수 있다.

--- src/main/java/oss/springmvc/basic/requestmapping/MappingController.java
+++ src/main/java/oss/springmvc/basic/requestmapping/MappingController.java
@@ -2,10 +2,7 @@ package oss.springmvc.basic.requestmapping;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController //  @RestController @Controller 는 반환 값이 String 이면 뷰 이름으로 인식된다. 그래서 뷰를 찾고 뷰가 랜더링 된다.
 
@@ -41,6 +38,11 @@ public class MappingController {
         return "ok";
     }
 
-
-
+    //PathVariable(경로 변수) 사용 많이 사용된다.
+    @GetMapping("/mapping/{userId}")
+    // @RequestMapping 은 URL 경로를 템플릿화 할 수 있는데, @PathVariable 을 사용하면 매칭 되는 부분을 편리하게 조회할 수 있다.@PathVariable 의 이름과 파라미터 이름이 같으면 생략할 수 있다.
+    public String mappingPath(@PathVariable("userId")String data){
+        log.info("mappingPath userId={}",data);
+        return "ok";
+    }
 }
