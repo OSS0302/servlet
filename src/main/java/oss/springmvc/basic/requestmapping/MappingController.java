1fcc0951cd9dc33dfe95f3444a6c9e74e55b6c3b
ohsangsoo <tkdtn3264@gmail.com>
2023-05-10 20:43:22 +0900

    대부분의 속성을 배열[] 로 제공하므로 다중 설정이 가능하다 basic1 basic

--- src/main/java/oss/springmvc/basic/requestmapping/MappingController.java
+++ src/main/java/oss/springmvc/basic/requestmapping/MappingController.java
@@ -5,12 +5,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController //  @RestController @Controller 는 반환 값이 String 이면 뷰 이름으로 인식된다. 그래서 뷰를 찾고 뷰가 랜더링 된다.
+
 public class MappingController {
 
     private Logger log= LoggerFactory.getLogger(getClass());
 
-    @RequestMapping("/hello-basic")
+    @RequestMapping({"/hello-basic","hello-basic1"})
     public String helloBasic(){
         log.info("helloBasic");
         return  "ok";
