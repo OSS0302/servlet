e0dd5a6d396c0e22d43ad1cc92728503aca79c47
ohsangsoo <tkdtn3264@gmail.com>
2023-05-10 20:55:52 +0900

    편리한 축약 어노테이션 을 코드

--- src/main/java/oss/springmvc/basic/requestmapping/MappingController.java
+++ src/main/java/oss/springmvc/basic/requestmapping/MappingController.java
@@ -5,7 +5,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
+/**
+ * 편리한 축약 애노테이션 (코드보기) * @GetMapping
+ * @PostMapping
+ * @PutMapping
+ * @DeleteMapping
+ * @PatchMapping
+ */
 @RestController //  @RestController @Controller 는 반환 값이 String 이면 뷰 이름으로 인식된다. 그래서 뷰를 찾고 뷰가 랜더링 된다.
 
 public class MappingController {
