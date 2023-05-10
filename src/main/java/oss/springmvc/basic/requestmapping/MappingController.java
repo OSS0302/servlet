b6a5727b7c7cad0153b87c790d10aeba073ec771
ohsangsoo <tkdtn3264@gmail.com>
2023-05-10 20:57:59 +0900

    mappingGetv1 추가

--- src/main/java/oss/springmvc/basic/requestmapping/MappingController.java
+++ src/main/java/oss/springmvc/basic/requestmapping/MappingController.java
@@ -2,25 +2,31 @@ package oss.springmvc.basic.requestmapping;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-/**
- * 편리한 축약 애노테이션 (코드보기) * @GetMapping
- * @PostMapping
- * @PutMapping
- * @DeleteMapping
- * @PatchMapping
- */
+
 @RestController //  @RestController @Controller 는 반환 값이 String 이면 뷰 이름으로 인식된다. 그래서 뷰를 찾고 뷰가 랜더링 된다.
 
 public class MappingController {
 
-    private Logger log= LoggerFactory.getLogger(getClass());
+    private Logger log = LoggerFactory.getLogger(getClass());
 
-    @RequestMapping(value = "/hello-basic",method = RequestMethod.GET )
-    public String helloBasic(){
+    @RequestMapping(value = "/hello-basic", method = RequestMethod.GET)
+    public String helloBasic() {
         log.info("helloBasic");
-        return  "ok";
+        return "ok";
     }
+
+    /**
+     * method 특정 HTTP 메서드 요청만 허용
+     * GET, HEAD, POST, PUT, PATCH, DELETE
+     */
+    @RequestMapping(value = "/mapping-get-v1", method = RequestMethod.GET)
+    public String mappingGetV1() {
+        log.info("mappingGetV1");
+        return "ok";
+    }
+
 }
