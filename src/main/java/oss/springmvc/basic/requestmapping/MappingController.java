7c724aea242773669b00a47254996274359103fb
ohsangsoo <tkdtn3264@gmail.com>
2023-05-10 20:54:00 +0900

    RequestMethod를 get으로 설정

--- src/main/java/oss/springmvc/basic/requestmapping/MappingController.java
+++ src/main/java/oss/springmvc/basic/requestmapping/MappingController.java
@@ -3,6 +3,7 @@ package oss.springmvc.basic.requestmapping;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController //  @RestController @Controller 는 반환 값이 String 이면 뷰 이름으로 인식된다. 그래서 뷰를 찾고 뷰가 랜더링 된다.
@@ -11,7 +12,7 @@ public class MappingController {
 
     private Logger log= LoggerFactory.getLogger(getClass());
 
-    @RequestMapping({"/hello-basic","hello-basic1"})
+    @RequestMapping(value = "/hello-basic",method = RequestMethod.GET )
     public String helloBasic(){
         log.info("helloBasic");
         return  "ok";
