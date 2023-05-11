c7369eace9a4d8c1bd6d455b46343fe975d002aa
ohsangsoo <tkdtn3264@gmail.com>
2023-05-11 16:36:17 +0900

    스프링이 제공하는 @RequestParam 을 사용하면 요청 파라미터를 매우 편리하게 사용할 수 있다.

--- src/main/java/oss/springmvc/basic/request/RequestParamController.java
+++ src/main/java/oss/springmvc/basic/request/RequestParamController.java
@@ -3,6 +3,8 @@ package oss.springmvc.basic.request;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -19,6 +21,25 @@ public class RequestParamController {
         log.info("username={},age={}",username,age );
 
         response.getWriter().write("ok");
+    }
+
+    /**
+     * @RequestParam 사용
+     * - 파라미터 이름으로 바인딩
+     **/
+
+    @ResponseBody //View 조회를 무시하고, HTTP message body에 직접 해당 내용 입력
+    @RequestMapping("/request-param-v2")
+    public String requestParamv2(
+            @RequestParam("username")String username,
+            @RequestParam("age")int age){
+
+        log.info("username={},age = {}",username, age); //로그로 유저이름과 나이 뽑아내기
+
+                return "ok";// 일반적으로 반환 하면 뷰 리졸버 찾기 때문에 ok라는 html 를 찾는다.
+
+
+
     }
 
 }
