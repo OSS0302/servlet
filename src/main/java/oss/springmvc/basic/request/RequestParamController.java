3a805d7f18ddcbec85b0457857d1f5d8a2705a83
ohsangsoo <tkdtn3264@gmail.com>
2023-05-11 16:41:21 +0900

    request-param-v3는 HTTP 파라미터 이름이 변수 이름과 같으면 @RequestParam(name="xx") 생략 가능하다

--- src/main/java/oss/springmvc/basic/request/RequestParamController.java
+++ src/main/java/oss/springmvc/basic/request/RequestParamController.java
@@ -37,9 +37,18 @@ public class RequestParamController {
         log.info("username={},age = {}",username, age); //로그로 유저이름과 나이 뽑아내기
 
                 return "ok";// 일반적으로 반환 하면 뷰 리졸버 찾기 때문에 ok라는 html 를 찾는다.
-
-
-
     }
+    /**
+     * @RequestParam 사용
+     * HTTP 파라미터 이름이 변수 이름과 같으면 @RequestParam(name="xx") 생략 가능 */
+    @ResponseBody
+    @RequestMapping("/request-param-v3")
+    public String requestParamV3(
+            @RequestParam String username, // RequestParam를 이름을 생략이가능하다.
+            @RequestParam int age) {       // 나이도 생략 가능하다.
+        log.info("username={}, age={}", username, age); // 파라미터 네임같아야지 생략이 가능하다
+        return "ok";
+    }
+
 
 }
