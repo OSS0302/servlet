282141221c48d6a49358077ff0c066b089f4055e
ohsangsoo <tkdtn3264@gmail.com>
2023-05-13 17:27:11 +0900

    responseBodyJsonV1 ResponseEntity 를 반환한다. HTTP 메시지 컨버터를 통해서 JSON 형식으로 변환되어서 반환된다.

--- src/main/java/oss/springmvc/basic/response/ResponseBodyController.java
+++ src/main/java/oss/springmvc/basic/response/ResponseBodyController.java
@@ -33,6 +33,15 @@ public class ResponseBodyController {
         return "ok";
     }
 
+    @GetMapping("/response-body-json-v1")
+    public ResponseEntity<HelloData> responseBodyJsonV1(){
+        HelloData helloData = new HelloData();
+        helloData.setUsername("UserA");
+        helloData.setAge(20);
+
+        return new ResponseEntity<>(helloData, HttpStatus.OK);
+
+    }
 
 
 
