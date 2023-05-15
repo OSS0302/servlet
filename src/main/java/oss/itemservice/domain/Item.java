50bb05556f92ed46b6c07fd05d2274505f7cc561
ohsangsoo <tkdtn3264@gmail.com>
2023-05-15 18:59:56 +0900

    아이템클래스  생성

--- src/main/java/oss/itemservice/domain/Item.java
+++ src/main/java/oss/itemservice/domain/Item.java
@@ -1,22 +1,19 @@
 package oss.itemservice.domain;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.extern.slf4j.Slf4j;
 
 @Data
-public class item {
+public class Item {
 
       private Long id; // 회원 아이디
       private String itemName; // 상품 이름
       private Integer priace; // 가격
       private Integer quantity; // 수량
 
-    public item() {
+    public Item() {
     }
 
-    public item(String itemName, Integer priace, Integer quantity) {
+    public Item(String itemName, Integer priace, Integer quantity) {
         this.itemName = itemName;
         this.priace = priace;
         this.quantity = quantity;
