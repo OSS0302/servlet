cdd6f2c38cfe778c0bc9f586b304d204ad41ecbd
ohsangsoo <tkdtn3264@gmail.com>
2023-05-16 17:42:33 +0900

    @RequiredArgsConstructor 어노테이션을 쓰면 자동으로 BasicItemController(ItemRepository itemRepository) 생성자를 만들어준다.

--- src/main/java/oss/itemservice/web/basic/BasicItemController.java
+++ src/main/java/oss/itemservice/web/basic/BasicItemController.java
@@ -1,5 +1,6 @@
 package oss.itemservice.web.basic;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,11 +8,12 @@ import oss.itemservice.repository.ItemRepository;
 
 @Controller
 @RequestMapping("/basic/items")
+@RequiredArgsConstructor
 public class BasicItemController {
     private  final ItemRepository itemRepository;
 
-    @Autowired
-    public BasicItemController(ItemRepository itemRepository){
-        this.itemRepository =itemRepository;
-    }
+//    @Autowired
+//    public BasicItemController(ItemRepository itemRepository){
+//        this.itemRepository =itemRepository;
+//    }
 }
