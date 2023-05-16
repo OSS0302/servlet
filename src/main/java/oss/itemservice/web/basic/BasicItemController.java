5aff1741a16bfa911dfcd03e75bfa0f8fe541713
ohsangsoo <tkdtn3264@gmail.com>
2023-05-16 17:46:35 +0900

    items 상품목록 controller 만들기

--- src/main/java/oss/itemservice/web/basic/BasicItemController.java
+++ src/main/java/oss/itemservice/web/basic/BasicItemController.java
@@ -3,8 +3,13 @@ package oss.itemservice.web.basic;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import oss.itemservice.domain.Item;
 import oss.itemservice.repository.ItemRepository;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/basic/items")
@@ -12,8 +17,10 @@ import oss.itemservice.repository.ItemRepository;
 public class BasicItemController {
     private  final ItemRepository itemRepository;
 
-//    @Autowired
-//    public BasicItemController(ItemRepository itemRepository){
-//        this.itemRepository =itemRepository;
-//    }
+    @GetMapping
+    public String items(Model model ){
+        List<Item> items = itemRepository.findAll(); // 상품 전체 조회 리스트
+        model.addAttribute("items",items);// 이름: items 데이터 items를 모델 실어서 보낸다.
+        return "basic/item";
+    }
 }
